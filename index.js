54da1aae86250ecc20675dfb6cbe5e9dc1f36cc2
Kelvin Nguyen <kelvinnuci@gmail.com>
2017-01-03 00:34:26 -0800

    Complete beta version of resume theme with proper styling for the about section, address formatting, and social media styling.

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 'use strict';
 
-var fs = require('fs');
-var handlebars = require('handlebars');
-var handlebarsWax = require('handlebars-wax');
-var Handlebars = handlebarsWax(handlebars);
+const
+    fs = require('fs'),
+    handlebars = require('handlebars'),
+    handlebarsWax = require('handlebars-wax'),
+    addressFormat = require('address-format'),
+    Swag = require('swag');
+
+Swag.registerHelpers(handlebars);
+
+handlebars.registerHelper({
+    removeProtocol: function (url) {
+        return url.replace(/.*?:\/\//g, '');
+    },
+
+    concat: function() {
+        let res = '';
+
+        for(let arg in arguments){
+            if (typeof arguments[arg] !== 'object') {
+                res += arguments[arg];
+            }
+        }
+
+        return res;
+    },
+
+    formatAddress: function(address, city, region, postalCode, countryCode) {
+        let addressList = addressFormat({
+            address: address,
+            city: city,
+            subdivision: region,
+            postalCode: postalCode,
+            countryCode: countryCode
+        });
+
+
+        return addressList.join('<br/>');
+    }
+});
+
 
 function render(resume) {
-    var dir = __dirname + '/public';
-    var css = fs.readFileSync(dir + '/styles/main.css', 'utf-8');
-    var resumeTemplate = fs.readFileSync(dir + '/views/resume.hbs', 'utf-8');
+    let dir = __dirname + '/public',
+        css = fs.readFileSync(dir + '/styles/main.css', 'utf-8'),
+        resumeTemplate = fs.readFileSync(dir + '/views/resume.hbs', 'utf-8');
+
+    let Handlebars = handlebarsWax(handlebars);
 
     Handlebars.partials(dir + '/views/partials/**/*.{hbs,js}');
     Handlebars.partials(dir + '/views/components/**/*.{hbs,js}');

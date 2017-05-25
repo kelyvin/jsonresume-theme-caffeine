24cd4bf31e2c68743b3ec90e01c5651aa439715e
phinguyen712 <phinguyen712@gmail.com>
2017-05-25 11:21:52 -0700

    gulp resume now exports resume.pdf

--- gulp/tasks/resume.js
+++ gulp/tasks/resume.js
@@ -1,13 +1,13 @@
 'use strict';
 const
     gulp = require('gulp'),
-     exporter= require('../../lib/exporter');
+    exporter= require('../../lib/exporter');
 
 //pull json data from resume.json and render an html and pdf version of resume
 gulp.task('resume', function () {
-    const readPathResume = './app/views/resume.hbs',
+    const
+        readPathResume = './app/views/resume.hbs',
         writePathResume = './public/resume.html';
-
     exporter.createResume(readPathResume,writePathResume,function(err){
         if(err){
             throw(err);

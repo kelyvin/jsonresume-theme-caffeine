dd68ebb03fb50d6e03807dee057e63627aaeb7a8
phinguyen712 <phinguyen712@gmail.com>
2017-05-25 11:08:33 -0700

    export now create resume.pdf file

--- gulp/tasks/resume.js
+++ gulp/tasks/resume.js
@@ -3,10 +3,20 @@ const
     gulp = require('gulp'),
      exporter= require('../../lib/exporter');
 
-//pull json data from resume.json and render an resume version of
+//pull json data from resume.json and render an html and pdf version of resume
 gulp.task('resume', function () {
-  const readPathResume = './app/views/resume.hbs',
+    const readPathResume = './app/views/resume.hbs',
         writePathResume = './public/resume.html';
 
-  exporter.createResume(readPathResume,writePathResume);
+    exporter.createResume(readPathResume,writePathResume,function(err){
+        if(err){
+            throw(err);
+        }else{
+            exporter.createPdf(function(err){
+                if(err){
+                    throw(err);
+                }
+            });
+        }
+    });
 });
